46435f6fd9d67d802a69c9b78dcc4cf31c87ba28
Adrian Uryga <75134645+Adolsik@users.noreply.github.com>
2023-08-01 13:38:01 +0200

    v1.0.1b
    Script now does not redirect user in 1000 firee tickets battle; Script is faster than before

--- index.js
+++ index.js
@@ -8,17 +8,9 @@
     // Callback function to execute when mutations are observed
     const callback = (mutationList, observer) => {
         for (const mutation of mutationList) {
-            if(mutation.addedNodes.length){
-                // info about the new battle
-                info = mutation.addedNodes[0].innerText
-
-                // link to the new battle
-                battle_link = mutation.addedNodes[0].querySelector('a').href
-
-                // if battle is free then app will open it
-                if(info.includes("FREE")) {
-                    window.location.href = battle_link
-                } 
+            if(mutation.addedNodes[0].querySelector('span[class="text-center text-sm font-bold text-green"]')){
+                // redirect to the new battle
+                window.location.href = mutation.addedNodes[0].querySelector('a').href
             }
         }
     };
